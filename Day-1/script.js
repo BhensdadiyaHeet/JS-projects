f1dc1867f1332f4704db3247fcc1310b9681ee06
BhensdadiyaHeet <heetbhesdadia1912@gmail.com>
2025-06-10 10:06:06 +0530

    project

---
+++ Day-1/script.js
@@ -0,0 +1,46 @@
+// console.log("Hello JS")
+// console.warn("This is a Warninng")
+// console.error("This is a error")
+
+// alert("This is an alert")
+// confirm("This is a confirmation box")
+// prompt("Enter your name : ")
+
+
+
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+
+// let num1 = prompt('Enter num 1')
+// let num2 = prompt('Enter num 2')
+
+// alert(num1+num2)
+
+
+
+// let num1 = Number(prompt('Enter num 1'))
+// let num2 = Number(prompt('Enter num 2'))
+
+// alert(num1+num2)
+
+
+// let name1 = prompt('Enter your first name')
+// let name2 = prompt('Enter your last name')
+
+// alert(name1+" "+name2)
+
+
+
+
+
+// let year = prompt("Enter your birth year");
+// alert(2025-year)
+
+
+
+
+
+// let naam = "Heet"
+// let str = `My name is ${naam}`
+// console.log(str)
